f90684fcc3a195382f241f771291e397a59c5669
Qin Li <30631553+liblaf@users.noreply.github.com>
2022-12-06 19:15:51 +0800

    fix(latexindent): add --modifylinebreaks & --GCString

--- scripts/template.sh
+++ scripts/template.sh
@@ -3,17 +3,19 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-if command -v rich >/dev/null 2>&1; then
-  function info() {
-    rich --print "[bold bright_blue]${*}"
-  }
-else
-  function info() {
+function exists() {
+  command -v "${@}" > /dev/null 2>&1
+}
+
+function info() {
+  if exists rich; then
+    rich --print --style "bold bright_blue" "${*}"
+  else
     echo -e -n "\x1b[1;94m"
     echo -n "${*}"
     echo -e "\x1b[0m"
-  }
-fi
+  fi
+}
 
 function call() {
   info "+ ${*}"
@@ -24,10 +26,10 @@ cd "$(git rev-parse --show-toplevel || echo .)"
 REPO_NAME="$(basename "$(pwd)")"
 
 description="${*}"
-echo "# ${REPO_NAME}" >"README.md"
-if [[ -n "${description}" ]]; then
-  echo "" >>"README.md"
-  echo "${description}" >>"README.md"
+echo "# ${REPO_NAME}" > "README.md"
+if [[ -n ${description} ]]; then
+  echo "" >> "README.md"
+  echo "${description}" >> "README.md"
 fi
 
 files=(
@@ -35,14 +37,14 @@ files=(
   "pyproject.toml"
 )
 for file in "${files[@]}"; do
-  call sed --in-place "s/template/${REPO_NAME}/g" "${file}"
+  call sed --in-place "s@template@${REPO_NAME}@g" "${file}"
 done
 
-call sed --in-place "s/description = \"Repository Template\"/description = \"${description}\"/g" pyproject.toml
+call sed --in-place "s@description =.*@description = \"${description}\"@g" pyproject.toml
 
 call gh repo edit --description "${description}"
 call gh repo edit --homepage "https://liblaf.github.io/${REPO_NAME}/"
 
 call git add --all
-call git commit --message "build: initialize" --verify --gpg-sign
+call git commit --message "build: initialize" --gpg-sign
 call git push
